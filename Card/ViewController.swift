8feae1efc9c9aafc3fa666c82e5c724d008a360f
cuoituoi1996 <dinhquocanh1996@gmail.com>
2021-05-15 03:05:26 -0700

    wip, double flip card back down if there are 2 face up cards

--- Card/ViewController.swift
+++ Card/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // make the card really big if they match, and disappear!
+    
     var cardDeck = CardDeck()
     var cards = [Card]()
     @IBOutlet private var cardViews: [CardView]!
+    
+    private var faceUpCardViews : [CardView] {
+        return cardViews.filter { $0.isFaceUp && !$0.isHidden }
+    }
     
     override func viewDidLoad() {
         
@@ -24,19 +30,33 @@ class ViewController: UIViewController {
             let card = cards.remove(at: Int.random(in: 0..<cards.count))
             cardView.rank = card.rank.order
             cardView.suit = card.suit.rawValue
-            cardView.isFaceUp = true
+            cardView.isFaceUp = false
             cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
         }
     }
-    
-    
     
     @objc func flipCard(_ sender: UITapGestureRecognizer) {
         switch sender.state {
         case .ended:
 //            print("Just tapped on card number: \(sender.view!)")
-            let tappedCard = sender.view as? CardView
-            tappedCard!.isFaceUp = !tappedCard!.isFaceUp
+            if let tappedCard = sender.view as? CardView {
+                UIView.transition(with: tappedCard,
+                                  duration: 0.4,
+                                  options: [.transitionFlipFromLeft],
+                                  animations: {tappedCard.isFaceUp = !tappedCard.isFaceUp},
+                                  completion: { finished in
+                                    if self.faceUpCardViews.count >= 2 {
+                                        self.faceUpCardViews.forEach { cardView in
+                                            UIView.transition(with: cardView,
+                                                              duration: 0.3,
+                                                              options: [.transitionFlipFromLeft],
+                                                              animations: {cardView.isFaceUp = !cardView.isFaceUp}
+                                            )
+                                        }
+                                    }
+                                  }
+                )
+            }
         default: break
         }
     }
