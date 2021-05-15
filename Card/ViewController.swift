0407378012074b07704097c409304cbd0052ae98
cuoituoi1996 <dinhquocanh1996@gmail.com>
2021-05-15 02:08:15 -0700

    initial commit, more to come

--- Card/ViewController.swift
+++ Card/ViewController.swift
@@ -9,40 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // probably will need to have like, multiple cardDeck?
-    // each UIItem is a cardDeck of its own...
     var cardDeck = CardDeck()
+    var cards = [Card]()
+    @IBOutlet private var cardViews: [CardView]!
     
-    @IBOutlet weak var cardView: CardView! {
-        didSet {
-
-            if let card = cardDeck.drawRandomCard() {
-                cardView.rank = card.rank.order
-                cardView.suit = card.suit.rawValue
-            }
-
-            let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipeGestureRecognizer.direction = [.left, .right]
-            let pinchGestureRecognizer = UIPinchGestureRecognizer(target: cardView, action: #selector(CardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            cardView.addGestureRecognizer(swipeGestureRecognizer)
-            cardView.addGestureRecognizer(pinchGestureRecognizer)
+    override func viewDidLoad() {
+        
+        for _ in 1...((cardViews.count+1)/2) {
+            let card = cardDeck.drawRandomCard()!
+            cards += [card, card]
+        }
+        
+        for cardView in cardViews {
+            let card = cards.remove(at: Int.random(in: 0..<cards.count))
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            cardView.isFaceUp = true
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
         }
     }
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+    
+    
+    @objc func flipCard(_ sender: UITapGestureRecognizer) {
         switch sender.state {
         case .ended:
-            cardView.isFaceUp = !cardView.isFaceUp
+//            print("Just tapped on card number: \(sender.view!)")
+            let tappedCard = sender.view as? CardView
+            tappedCard!.isFaceUp = !tappedCard!.isFaceUp
         default: break
         }
     }
     
-    @objc func nextCard() {
-        if let card = cardDeck.drawRandomCard() {
-            cardView.rank = Int(card.rank.order)
-            cardView.suit = String(card.suit.rawValue)
-            cardView.isFaceUp = false
-        }
-    }
+//    // don't need this function for now
+//    @objc func nextCard() {
+//        if let card = cardDeck.drawRandomCard() {
+//            cardView.rank = Int(card.rank.order)
+//            cardView.suit = String(card.suit.rawValue)
+//            cardView.isFaceUp = false
+//        }
+//    }
+
+    
+//        @IBOutlet weak var cardView: CardView! {
+//        didSet {
+//
+//            if let card = cardDeck.drawRandomCard() {
+//                cardView.rank = card.rank.order
+//                cardView.suit = card.suit.rawValue
+//            }
+//
+//            let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+//            swipeGestureRecognizer.direction = [.left, .right]
+//            cardView.addGestureRecognizer(swipeGestureRecognizer)
+//            let pinchGestureRecognizer = UIPinchGestureRecognizer(target: cardView, action: #selector(CardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+//            cardView.addGestureRecognizer(pinchGestureRecognizer)
+//        }
+//    }
+
 }
 
